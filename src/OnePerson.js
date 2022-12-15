08614dfb9a2becf94f40146b2ae578ecb174f189
melkit235 <verghel.iliya@gmail.com>
2022-12-16 00:14:10 +0300

    add

--- src/OnePerson.js
+++ src/OnePerson.js
@@ -1,22 +1,27 @@
 import { Timeline, TimelineItem }  from 'vertical-timeline-component-for-react';
+import Carousel from 'carousel-react-rcdev'
+
 import WritersInfo from './WritersInfo';
+
+import {useParams, Link} from "react-router-dom";
+
 
 
 function OnePerson(){
     const params = useParams();
     const id = params.number;
 
-    const writer = WritersInfo.writer.find(p=>p.number == id);
-    const wrInfo = WritersInfo.wrInfo.find(p=>p.number == id);
+    const writer = WritersInfo.writer.find(w=>w.number == id);
+    const wrInfo = WritersInfo.wrInfo.find(w=>w.number == id);
 
     return(
         <div>
-            <div className="Info">
+            <div className="">
                 <img alt="" src={require(`./img/${writer.fileName}/por.jpg`)} height="400" width=""/>
-                <div className="Text">
+                <div className="">
                 <h1>{writer.name}</h1>
                 <h2>{wrInfo.birthDate} - {wrInfo.deathDate}</h2>
-                <div className="shortInfoWrap">
+                <div className="">
                     <p>{wrInfo.shortInfo}</p>
                 </div>  
                 </div>
@@ -24,13 +29,40 @@ function OnePerson(){
 
             <Timeline lineColor={'#ddd'}>
                 {
-                    wrInfo.events.map(p =>(
-                        <TimelineItem>
+                    wrInfo.events.map(w =>(
+                        <TimelineItem
+                        style = {{color: '#000'}}
+                        bodyContainerStyle={{
+                            backgraund: '#ddd'
+                        }}
+                        dateText = {w.year}>
                             <p>p.description</p>
                         </TimelineItem>   
                     ))
                 }       
             </Timeline>
+
+            <Carousel>
+            {
+                phtData.gallery.map(w => (
+                <Carousel.Item className="">
+                    <img src={require(`./img/${writer.fileName}/${w}`)} alt=""/>
+                </Carousel.Item>
+                ))
+            }    
+            </Carousel>
+
+
+            <div className="">
+                <div className="">
+                    <iframe className="" src={phtData.videoLink} frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
+                    </iframe>
+                </div>
+                    <iframe src={phtData.maplink} className=""
+                        allowfullscreen="" referrerpolicy="no-referrer-when-downgrade">
+                    </iframe>
+                </div>
         </div>
     );
 }
