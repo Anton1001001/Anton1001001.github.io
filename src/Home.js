a348b41fc11af378ef34d22518d73a24ad48c8e2
vashka04 <vashkevichartem@mail.ru>
2022-12-15 23:29:43 +0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,34 @@
 import React from "react";
-import Footer from './Components/Footer'
-import { Container } from "react-bootstrap";
+import Footer from './Components/Footer';
+import WritersInfo from "./WritersInfo";
+import {Button} from 'react-bootstrap';
+import {Link} from "react-router-dom";
+//import "./Home.css"
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  let dayWr = getRandomInt(1,6);
+  const randomPerson = WritersInfo.writer.find(w=>w.number === dayWr);
+  const wrtInfo = WritersInfo.wrInfo.find(w=>w.number === dayWr);
 
 export const Home = () => (
     <>
-    <Container style={{backgroundColor: "aqua"}}>
-
-    </Container>
+       
+            
+            <img alt="" src={require(`./img/${randomPerson.fileName}/por.jpg`)} height="400" width="" className="portreit"/> 
+            
+            <h1>{randomPerson.name}</h1>
+            <h2>{wrtInfo.birthDate} - {wrtInfo.deathDate}</h2>
+             
+            <p>{wrtInfo.shortInfo}</p>
+              
+            <Button variant="outline-dark" as={Link} to={`/list/${randomPerson.number}`}>l_more</Button>
+                 
     <Footer/>
     </>
 )
+
+
+
+
